a705b146f3ca4c3ea6dedd9a17ca70969b7c8d03
Osvaldo Luna <osvaldounam18@gmail.com>
2017-11-16 20:15:17 -0600

    Se ingresa una matriz creada por el usuario y se calcula la suma de su diagonal principal

---
+++ ConsoleApp2/Diagonal.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int mat, suma = 0;
+
+            Console.WriteLine("Tamaño deseado para la Matriz:");
+            mat = int.Parse(Console.ReadLine());
+            int[,] val = new int[mat, mat];
+            Console.WriteLine("Llenando");
+            Console.WriteLine("--------------------------------------------------");
+
+            for (int r = 0; r < mat; r++)
+            {
+                for (int c = 0; c < mat; c++)
+                {
+                    Console.WriteLine($"Cual es el valor de [{r},{c}]");
+                    val[r, c] = int.Parse(Console.ReadLine());
+
+                    if (r == c)
+                    {
+                        suma += val[r, c];
+                    }
+
+                }
+            }
+
+            Console.WriteLine("Hemos terminado");
+            Console.WriteLine("-----------------");
+            Console.WriteLine("La Matriz queda de la sigiente manera: ");
+            for(int m=0; m<mat ;m++ )
+            {
+                Console.Write("[");
+                for (int n=0;n<mat ;n++ )
+                {
+                    Console.Write("{0}",val[m,n]);  
+                }
+                Console.WriteLine("]");
+            }
+            Console.WriteLine("La Suma De Su Diagonal es: {0}", suma);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tama, suma = 0;
+
+            Console.WriteLine("De que tamaño deseas generar la matriz:");
+            tama = int.Parse(Console.ReadLine());
+            int[,] alum = new int[tama, tama];
+            Console.WriteLine("Empezemos el Llenado");
+            Console.WriteLine("--------------------------------------------------");
+
+            for (int i = 0; i < tama; i++)
+            {
+                for (int j = 0; j < tama; j++)
+                {
+                    Console.WriteLine($"Cual es el valor de {i}-{j}");
+                    alum[i, j] = int.Parse(Console.ReadLine());
+
+                    if (i == j)
+                    {
+                        suma += alum[i, j];
+                    }
+
+                }
+            }
+
+            Console.WriteLine("Llenado Finalizado");
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine("La Suma De Su Diagonal es: {0}", suma);
+
+            Console.ReadKey();
+        }
+    }
+}
